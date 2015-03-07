2f323566a38bfa476038207dda5419ba0e3551a5
Pedro Costa <dev@iampfac.com>
2015-03-07 01:31:10 +0000

    Adds FizzBuzz case

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,15 +1,19 @@
 defmodule FizzBuzz do
 
-  def fizzbuzz(3) do
+  def fizzbuzz(n) when rem(n, 15) == 0 do
+    "FizzBuzz"
+  end
+
+  def fizzbuzz(n) when rem(n, 3) == 0 do
     "Fizz"
   end
 
-  def fizzbuzz(5) do
+  def fizzbuzz(n) when rem(n, 5) == 0 do
     "Buzz"
   end
 
   def fizzbuzz(n) do
     n
   end
-  
+
 end
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -5,12 +5,31 @@ defmodule FizzBuzzTest do
     assert FizzBuzz.fizzbuzz(3) == "Fizz"
   end
 
+  test "6 is fizz" do
+    assert FizzBuzz.fizzbuzz(6) == "Fizz"
+  end
+
   test "5 is buzz" do
     assert FizzBuzz.fizzbuzz(5) == "Buzz"
   end
 
-  test "any other is n" do
-    n = :random.uniform 100
-    assert FizzBuzz.fizzbuzz(n) == n
+  test "10 is buzz" do
+    assert FizzBuzz.fizzbuzz(10) == "Buzz"
+  end
+
+  test "15 is fizzbuzz" do
+    assert FizzBuzz.fizzbuzz(15) == "FizzBuzz"
+  end
+
+  test "30 is fizzbuzz" do
+    assert FizzBuzz.fizzbuzz(30) == "FizzBuzz"
+  end
+
+  test "1 is 1" do
+    assert FizzBuzz.fizzbuzz(1) == 1
+  end
+
+  test "2 is 2" do
+    assert FizzBuzz.fizzbuzz(2) == 2
   end
 end
