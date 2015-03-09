4ee18ddf31d15f06c325c94cd3d75d9e50965187
Pedro Costa <dev@iampfac.com>
2015-03-09 14:17:43 +0000

    Refactoring to be pretty

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,19 +1,11 @@
 defmodule FizzBuzz do
 
-  def fizzbuzz(n) when rem(n, 15) == 0 do
-    "FizzBuzz"
-  end
+  def fizzbuzz(n) when rem(n, 15) == 0, do: "FizzBuzz"
 
-  def fizzbuzz(n) when rem(n, 3) == 0 do
-    "Fizz"
-  end
+  def fizzbuzz(n) when rem(n, 3) == 0, do: "Fizz"
 
-  def fizzbuzz(n) when rem(n, 5) == 0 do
-    "Buzz"
-  end
+  def fizzbuzz(n) when rem(n, 5) == 0, do: "Buzz"
 
-  def fizzbuzz(n) do
-    n
-  end
+  def fizzbuzz(n), do: n
 
 end
